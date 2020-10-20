56b381d0406b01c62cd1c635d6b7e22cf53c86e4
monkka <mcarmojm@gmail.com>
2020-10-20 19:02:50 +0000

    Modificação no banco de dados

--- experiencein/settings.py
+++ experiencein/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!ui0_wsnt_%@kf@t@-$n@ub)*&*7@=-7k*rt^^x(v(^ep4#r4t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['monkka.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/home/monkka/experiencein/static'
 
 LOGIN_URL='/login/'
 LOGOUT_URL='/logout/'
